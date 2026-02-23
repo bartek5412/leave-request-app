20f77f94e622fc2cf29c19a53a942111116e9b77
Bartek <bartek5412@gmail.com>
2026-02-23 18:36:34 +0100

    wip

---
+++ app/login/page.tsx
@@ -0,0 +1,33 @@
+import { GalleryVerticalEnd } from "lucide-react"
+
+import { LoginForm } from "@/components/login-form"
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-row justify-center gap-2 md:justify-start">
+          <div className="flex items-center gap-2 font-medium ">
+            <img
+              alt="Logo fundacji"
+              className="h-50 w-auto"
+              src="/fcpbh_logo_kolor.svg"
+            />
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/app-background2.png"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
